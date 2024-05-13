6370aa6cc6d38585dbeb0689a83b8f77406a0438
Peter Kurhajec <61538034+PTKu@users.noreply.github.com>
2024-05-13 09:35:33 +0200

    Structuredtype initialize arrays fields (#313)
    * structured type array init
    
    * test expected additions
    
    ---------
    
    Co-authored-by: blazej.kuhajda <blazej.kuhajda@mts.sk>

--- src/AXSharp.compiler/tests/AXSharp.Compiler.CsTests/samples/units/expected/.g/POCO/struct_simple.g.cs
+++ src/AXSharp.compiler/tests/AXSharp.Compiler.CsTests/samples/units/expected/.g/POCO/struct_simple.g.cs
@@ -2,13 +2,21 @@ using System;
 
 namespace Pocos
 {
-    public partial class Motor
+    public partial class Motor : AXSharp.Connector.IPlain
     {
+        public Motor()
+        {
+        }
+
         public Boolean isRunning { get; set; }
     }
 
-    public partial class Vehicle
+    public partial class Vehicle : AXSharp.Connector.IPlain
     {
+        public Vehicle()
+        {
+        }
+
         public Motor m { get; set; } = new Motor();
         public Int16 displacement { get; set; }
     }
