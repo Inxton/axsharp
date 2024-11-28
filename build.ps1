d1342b4a86f9b2481385c0b9e572745c4e58b86d
PTKu <61538034+PTKu@users.noreply.github.com>
2024-11-28 06:57:39 +0100

    Add support for .NET 9 and centralize package version management

--- build.ps1
+++ build.ps1
@@ -1,3 +1,3 @@
 # run build
-dotnet run --project cake/Build.csproj -- $args
-exit $LASTEXITCODE;
+dotnet run --project cake/Build.csproj --framework net9.0 -- $args
+exit $LASTEXITCODE;vm43001Jetpow3r
