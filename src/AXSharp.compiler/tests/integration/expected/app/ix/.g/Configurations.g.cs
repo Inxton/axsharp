d56baeb38ac7bd3ec4807c16f328db69f85fe2f9
PTKu <61538034+PTKu@users.noreply.github.com>
2024-12-11 12:23:59 +0100

    commit expected output from integration

---
+++ src/AXSharp.compiler/tests/integration/expected/app/ix/.g/Configurations.g.cs
@@ -0,0 +1,29 @@
+using System;
+using AXSharp.Connector;
+using AXSharp.Connector.ValueTypes;
+using System.Collections.Generic;
+using AXSharp.Connector.Localizations;
+using AXSharp.Abstractions.Presentation;
+
+public partial class appTwinController : ITwinController
+{
+    public AXSharp.Connector.Connector Connector { get; }
+    public lib1.MyClass lib1_MyClass { get; }
+    public lib2.MyClass lib2_MyClass { get; }
+
+    partial void PreConstruct(AXSharp.Connector.ITwinObject parent, string readableTail, string symbolTail);
+    partial void PostConstruct(AXSharp.Connector.ITwinObject parent, string readableTail, string symbolTail);
+    public appTwinController(AXSharp.Connector.ConnectorAdapter adapter, object[] parameters)
+    {
+        this.Connector = adapter.GetConnector(parameters);
+        lib1_MyClass = new lib1.MyClass(this.Connector, "", "lib1_MyClass");
+        lib2_MyClass = new lib2.MyClass(this.Connector, "", "lib2_MyClass");
+    }
+
+    public appTwinController(AXSharp.Connector.ConnectorAdapter adapter)
+    {
+        this.Connector = adapter.GetConnector(adapter.Parameters);
+        lib1_MyClass = new lib1.MyClass(this.Connector, "", "lib1_MyClass");
+        lib2_MyClass = new lib2.MyClass(this.Connector, "", "lib2_MyClass");
+    }
+}
