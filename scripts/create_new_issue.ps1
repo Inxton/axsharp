ecec88b0815eb13bec77220d366f7599f181e03e
TK <61820360+TomKovac@users.noreply.github.com>
2025-02-13 19:27:42 +0100

    wip

--- scripts/create_new_issue.ps1
+++ scripts/create_new_issue.ps1
@@ -1,10 +1,29 @@
 param 
 (
-     [Parameter(Mandatory=$true)]
     [string]$IssueTitle, 
-     [Parameter(Mandatory=$true)]
     [string]$IssueBody 
 )
+
+if (-not $IssueTitle) 
+{
+    $IssueTitle = Read-Host "Please enter an issue title."
+}
+if (-not $IssueBody) 
+{
+    $IssueBody = Read-Host "Please enter an issue body."
+}
+
+
+if (-not $IssueTitle) 
+{
+    Write-Output "Issue title cannot be an empty string!"
+    exit 1
+}
+if (-not $IssueBody) 
+{
+    $IssueBody = " "
+}
+
 
 $issue = gh issue create --assignee "@me" --title "$IssueTitle" --body "$IssueBody" --project simatic-ax
 
@@ -14,11 +33,11 @@ if ($issue -match ".*/(\d+)$") {
     # Get the current script directory
     $scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 
-    # Construct the full path to SecondScript.ps1
-    $create_issue_branchScriptPath = Join-Path -Path $scriptDir -ChildPath "_create_issue_branch.ps1"
+    # Construct the full path to _create_issue_branch.ps1
+    $_create_issue_branchScriptPath = Join-Path -Path $scriptDir -ChildPath "_create_issue_branch.ps1"
 
-    # Call create_issue_branch.ps1 with the parameter IssueId
-    & $create_issue_branchScriptPath -IssueId $issueID
+    # Call _create_issue_branch.ps1 with the parameter IssueId and doNotCheckOldColumnName
+    & $_create_issue_branchScriptPath -IssueId $issueID -doNotCheckOldColumnName $True
 } else {
     Write-Output "No numeric ID found for this issue."
 }
--- src/AXSharp.compiler/src/ixc/Program.cs
+++ src/AXSharp.compiler/src/ixc/Program.cs
@@ -14,6 +14,9 @@ using AXSharp.Compiler;
 using AXSharp.Compiler.Cs.Onliner;
 using AXSharp.Compiler.Cs.Plain;
 using System.Text.Json;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.CSharp;
+
 
 namespace ixc;
 
@@ -90,6 +93,8 @@ public static class Program
         var sw = new System.Diagnostics.Stopwatch();
         sw.Start();
         project.Generate();
+        GetPlcResources(project.OutputFolder);
+
         sw.Stop();
         Log.Logger.Information($"Done in {TimeSpan.FromMilliseconds(sw.ElapsedMilliseconds)}");
         return project;
@@ -123,4 +128,77 @@ public static class Program
         }
     }
 
+    private static void GetPlcResources(string folder)
+    {
+        List<PlcResource> plcResources = new List<PlcResource>() 
+        {
+            new PlcResource("UnitContainerBase"),
+            new PlcResource("AXOpen.Core.AxoComponent"),
+            new PlcResource("AXOpen.Core.AxoSequencerContainer"),
+            new PlcResource("AXOpen.Core.AxoTask"),
+            new PlcResource("AXOpen.Data.AxoDataExchange"),
+            new PlcResource("AXOpen.Data.AxoDataFragmentExchange"),
+        };
+
+        string folderPOCO = Path.GetFullPath(Path.Combine(folder, ".g\\POCO\\"));
+        string plcResourcesPath = Path.GetFullPath(Path.Combine(folder, "PlcResources.json"));
+
+        var files = Directory.GetFiles(folderPOCO, "*.cs", SearchOption.AllDirectories);
+
+        foreach (var file in files)
+        {
+            string code = File.ReadAllText(file);
+
+            var syntaxTree = CSharpSyntaxTree.ParseText(code);
+
+            var root = syntaxTree.GetRoot();
+
+            foreach (PlcResource plcResource in plcResources)
+            {
+                var classDeclarations = root.DescendantNodes().OfType<ClassDeclarationSyntax>().Where(cls => ExtendsClass(cls, plcResource.BaseClassName));
+
+                foreach (var classDecl in classDeclarations)
+                {
+                    plcResource.Instances.Add(file.ToString().Replace(folderPOCO,"").Replace(".g.cs", "").Replace("\\", "."));
+                    plcResource.InstanceCount++;
+                }
+            }
+        }
+
+        if (File.Exists(plcResourcesPath))
+        {
+            try
+            {
+                File.Delete(plcResourcesPath);
+            }
+            catch (Exception ex){}
+        }
+
+        string json = Newtonsoft.Json.JsonConvert.SerializeObject(plcResources, Newtonsoft.Json.Formatting.Indented);
+
+        File.WriteAllText(plcResourcesPath, json);
+
+        Console.WriteLine($"Data written to {plcResourcesPath}");
+    }
+
+
+    private static bool ExtendsClass(ClassDeclarationSyntax classDecl, string baseClassName)
+    {
+        return classDecl.BaseList?.Types.Any(baseType => baseType.ToString().Contains(baseClassName)) ?? false;
+    }
+
+    
+    private class PlcResource
+    {
+        public string BaseClassName { get; set; }
+        public int InstanceCount { get; set; }
+        public List<string> Instances { get; set; }
+
+        public PlcResource(string baseClassName)
+        {
+            BaseClassName = baseClassName;
+            InstanceCount = 0;
+            Instances = new List<string>();
+        }
+    }
 }
