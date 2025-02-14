a69e3d12cebafedadc32624b52d26d6128d79c92
TK <61820360+TomKovac@users.noreply.github.com>
2025-02-14 09:32:00 +0100

    rewert program.cs in ixc

--- src/AXSharp.compiler/src/ixc/Program.cs
+++ src/AXSharp.compiler/src/ixc/Program.cs
@@ -14,9 +14,6 @@ using AXSharp.Compiler;
 using AXSharp.Compiler.Cs.Onliner;
 using AXSharp.Compiler.Cs.Plain;
 using System.Text.Json;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.CSharp;
-
 
 namespace ixc;
 
@@ -44,11 +41,11 @@ public static class Program
             {
                 var recoverCurrentDirectory = Environment.CurrentDirectory;
                 try
-                {                    
+                {
                     Log.ConfigureLogger(o.Versbosity);
 
                     Log.Logger.Verbose(JsonSerializer.Serialize(o));
-                    
+
                     Project = GenerateIxProject(o);
                 }
                 catch (Exception e)
@@ -79,7 +76,7 @@ public static class Program
     }
 
     private static AXSharpProject GenerateIxProject(Options options)
-    {        
+    {
         var axProjectFolder = string.IsNullOrEmpty(options.AxSourceProjectFolder)
             ? Environment.CurrentDirectory
             : options.AxSourceProjectFolder;
@@ -93,8 +90,6 @@ public static class Program
         var sw = new System.Diagnostics.Stopwatch();
         sw.Start();
         project.Generate();
-        GetPlcResources(project.OutputFolder);
-
         sw.Stop();
         Log.Logger.Information($"Done in {TimeSpan.FromMilliseconds(sw.ElapsedMilliseconds)}");
         return project;
@@ -114,7 +109,7 @@ public static class Program
                           "THIRD PARTY LICENSES CAN BE FOUND AT \n" +
                           "https://github.com/inxton/axsharp/blob/master/notices.md");
 
-        
+
         Console.ForegroundColor = ConsoleColor.Magenta;
         Console.WriteLine($"Using version '{LegalAcrobatics.StcVersion}' of stc.");
         Console.ForegroundColor = originalColor;
@@ -128,78 +123,4 @@ public static class Program
         }
     }
 
-    private static void GetPlcResources(string folder)
-    {
-        List<PlcResource> plcResources = new List<PlcResource>() 
-        {
-            //Additional "power demanding" base classes should be placed here
-            new PlcResource("UnitContainerBase"),
-            new PlcResource("AXOpen.Core.AxoComponent"),
-            new PlcResource("AXOpen.Core.AxoSequencerContainer"),
-            new PlcResource("AXOpen.Core.AxoTask"),
-            new PlcResource("AXOpen.Data.AxoDataExchange"),
-            new PlcResource("AXOpen.Data.AxoDataFragmentExchange"),
-        };
-
-        string folderPOCO = Path.GetFullPath(Path.Combine(folder, ".g\\POCO\\"));
-        string plcResourcesPath = Path.GetFullPath(Path.Combine(folder, "PlcResources.json"));
-
-        var files = Directory.GetFiles(folderPOCO, "*.cs", SearchOption.AllDirectories);
-
-        foreach (var file in files)
-        {
-            string code = File.ReadAllText(file);
-
-            var syntaxTree = CSharpSyntaxTree.ParseText(code);
-
-            var root = syntaxTree.GetRoot();
-
-            foreach (PlcResource plcResource in plcResources)
-            {
-                var classDeclarations = root.DescendantNodes().OfType<ClassDeclarationSyntax>().Where(cls => ExtendsClass(cls, plcResource.BaseClassName));
-
-                foreach (var classDecl in classDeclarations)
-                {
-                    plcResource.Instances.Add(file.ToString().Replace(folderPOCO,"").Replace(".g.cs", "").Replace("\\", "."));
-                    plcResource.InstanceCount++;
-                }
-            }
-        }
-
-        if (File.Exists(plcResourcesPath))
-        {
-            try
-            {
-                File.Delete(plcResourcesPath);
-            }
-            catch (Exception ex){}
-        }
-
-        string json = Newtonsoft.Json.JsonConvert.SerializeObject(plcResources, Newtonsoft.Json.Formatting.Indented);
-
-        File.WriteAllText(plcResourcesPath, json);
-
-        Console.WriteLine($"Data written to {plcResourcesPath}");
-    }
-
-
-    private static bool ExtendsClass(ClassDeclarationSyntax classDecl, string baseClassName)
-    {
-        return classDecl.BaseList?.Types.Any(baseType => baseType.ToString().Contains(baseClassName)) ?? false;
-    }
-
-    
-    private class PlcResource
-    {
-        public string BaseClassName { get; set; }
-        public int InstanceCount { get; set; }
-        public List<string> Instances { get; set; }
-
-        public PlcResource(string baseClassName)
-        {
-            BaseClassName = baseClassName;
-            InstanceCount = 0;
-            Instances = new List<string>();
-        }
-    }
 }
