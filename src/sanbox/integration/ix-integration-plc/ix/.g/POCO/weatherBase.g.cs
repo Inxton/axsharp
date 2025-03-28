24e5745644b349fbd55cf2e066f8d71fd1fe4ca8
PTKu <61538034+PTKu@users.noreply.github.com>
2025-03-28 15:30:48 +0100

    Updates to stc v9

--- src/sanbox/integration/ix-integration-plc/ix/.g/POCO/weatherBase.g.cs
+++ src/sanbox/integration/ix-integration-plc/ix/.g/POCO/weatherBase.g.cs
@@ -16,22 +16,22 @@ namespace Pocos
         public string Description { get; set; } = string.Empty;
         public string LongDescription { get; set; } = string.Empty;
 
-        [AXSharp.Connector.AddedPropertiesAttribute("AttributeName", "this has [ReadOnce()] attribute will be readon only once...")]
+        [AXSharp.Connector.AddedPropertiesAttribute("AttributeName", @"this has [ReadOnce()] attribute will be readon only once...")]
         public Int16 StartCounter { get; set; }
 
-        [AXSharp.Connector.AddedPropertiesAttribute("AttributeName", "[RenderIgnore()] must not be displayed!")]
+        [AXSharp.Connector.AddedPropertiesAttribute("AttributeName", @"[RenderIgnore()] must not be displayed!")]
         public string RenderIgnoreAllToghether { get; set; } = string.Empty;
 
-        [AXSharp.Connector.AddedPropertiesAttribute("AttributeName", "[RenderIgnore(''Control'')]")]
+        [AXSharp.Connector.AddedPropertiesAttribute("AttributeName", @"[RenderIgnore(''Control'')]")]
         public string RenderIgnoreWhenControl { get; set; } = string.Empty;
 
-        [AXSharp.Connector.AddedPropertiesAttribute("AttributeName", "[RenderIgnore(''Display'')]")]
+        [AXSharp.Connector.AddedPropertiesAttribute("AttributeName", @"[RenderIgnore(''Display'')]")]
         public string RenderIgnoreWhenDisplay { get; set; } = string.Empty;
 
-        [AXSharp.Connector.AddedPropertiesAttribute("AttributeName", "[RenderIgnore(''Control'', ''ShadowControl'')]")]
+        [AXSharp.Connector.AddedPropertiesAttribute("AttributeName", @"[RenderIgnore(''Control'', ''ShadowControl'')]")]
         public string RenderIgnoreWhenControlAndShadow { get; set; } = string.Empty;
 
-        [AXSharp.Connector.AddedPropertiesAttribute("AttributeName", "[RenderIgnore(''Display'', ''ShadowDisplay'')]")]
+        [AXSharp.Connector.AddedPropertiesAttribute("AttributeName", @"[RenderIgnore(''Display'', ''ShadowDisplay'')]")]
         public string RenderIgnoreWhenDisplayAndShadow { get; set; } = string.Empty;
     }
 }
