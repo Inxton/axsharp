8f2c9aaa32404b5dcaa0b06d5e280eab83cf9a48
blazej.kuhajda <blazej.kuhajda@mts.sk>
2025-04-01 09:40:04 +0200

    localization support
    added top row with combobox for localization

---
+++ src/sanbox/integration/ix-integration-blazor/Controllers/CultureController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using AXSharp.Connector;
+
+namespace ix_integration_blazor;
+
+[Route("/[controller]")]
+[ApiController]
+public class CultureController : ControllerBase
+{
+    public async Task<ActionResult> ChangeCulture([FromQuery] string culture)
+    {
+        if (culture != null)
+        {
+            HttpContext.Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(
+            new RequestCulture(culture, culture)));
+
+            CultureExtensions.Culture = new CultureInfo(culture);
+            Connector.SetCulture(CultureExtensions.Culture);
+
+        }
+
+        return Redirect("/");
+    }
+}
+
+public static class CultureExtensions
+{
+    public static CultureInfo Culture { get; set; } = CultureInfo.CurrentCulture ?? CultureInfo.DefaultThreadCurrentCulture ?? new CultureInfo("en-US");
+}
