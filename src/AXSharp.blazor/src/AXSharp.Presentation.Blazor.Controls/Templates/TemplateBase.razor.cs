532b0cf7385a74440b1c7c8b772246f62f611059
Branko Zachemsky <bzachemsky@gmail.com>
2026-03-04 15:14:35 +0200

    update operon update format of base

--- src/AXSharp.blazor/src/AXSharp.Presentation.Blazor.Controls/Templates/TemplateBase.razor.cs
+++ src/AXSharp.blazor/src/AXSharp.Presentation.Blazor.Controls/Templates/TemplateBase.razor.cs
@@ -1,4 +1,4 @@
-﻿using AXSharp.Connector.ValueTypes;
+using AXSharp.Connector.ValueTypes;
 using AXSharp.Presentation.Blazor.Controls.RenderableContent;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
