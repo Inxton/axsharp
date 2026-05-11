f621539c8e7b6084367a2305ff5707ff38bd5396
Branko Zachemsky <bzachemsky@gmail.com>
2026-05-11 12:31:54 +0200

    fix TIME property

--- src/AXSharp.blazor/src/AXSharp.Presentation.Blazor.Controls/Templates/Base/FormatConstants.cs
+++ src/AXSharp.blazor/src/AXSharp.Presentation.Blazor.Controls/Templates/Base/FormatConstants.cs
@@ -6,12 +6,70 @@ namespace AXSharp.Presentation.Blazor.Controls.Templates.Base
 {
     public static class FormatConstants
     {
-        public const string TimeFormat = @"ss\.FFF";
+        public static readonly string[] TimeFormats =
+            [
+                // MOST SPECIFIC -> LEAST SPECIFIC
 
-        public const string LTimeFormat = @"ss\.FFF";
+                // days
+                @"d\.h\:m\:s\.fffffff",
+                @"d\.h\:m\:s\.ffffff",
+                @"d\.h\:m\:s\.fffff",
+                @"d\.h\:m\:s\.ffff",
+                @"d\.h\:m\:s\.fff",
+                @"d\.h\:m\:s\.ff",
+                @"d\.h\:m\:s\.f",
+                @"d\.h\:m\:s\.",
 
-        public const string TimeOfDayFormat = @"hh\:mm\:ss";
+                @"d\.h\:m\:s",
 
-        public const string LTimeOfDayFormat = @"hh\:mm\:ss";
+                // hours
+                @"h\:m\:s\.fffffff",
+                @"h\:m\:s\.ffffff",
+                @"h\:m\:s\.fffff",
+                @"h\:m\:s\.ffff",
+                @"h\:m\:s\.fff",
+                @"h\:m\:s\.ff",
+                @"h\:m\:s\.f",
+                @"h\:m\:s\.",
+
+                @"h\:m\:s",
+
+                // minutes
+                @"m\:s\.fffffff",
+                @"m\:s\.ffffff",
+                @"m\:s\.fffff",
+                @"m\:s\.ffff",
+                @"m\:s\.fff",
+                @"m\:s\.ff",
+                @"m\:s\.f",
+                @"m\:s\.",
+
+                @"m\:s",
+
+                // seconds
+
+                @"s\.fffffff",
+                @"s\.ffffff",
+                @"s\.fffff",
+                @"s\.ffff",
+                @"s\.fff",
+                @"s\.ff",
+                @"s\.f",
+                @"s\.",
+
+                @"s",
+
+                // fractions
+
+                @"\.fffffff",
+                @"\.ffffff",
+                @"\.fffff",
+                @"\.ffff",
+                @"\.fff",
+                @"\.ff",
+                @"\.f",
+
+                @"c"
+            ];
     }
 }
